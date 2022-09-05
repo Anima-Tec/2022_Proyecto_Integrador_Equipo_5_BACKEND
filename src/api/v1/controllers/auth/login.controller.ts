33485e7f660aa1734317d73583c417956b73a646
Emilianoo03 <79849513+Emilianoo03@users.noreply.github.com>
2022-09-04 21:13:27 -0300

    fix validations (#4)

--- src/api/v1/controllers/auth/login.controller.ts
+++ src/api/v1/controllers/auth/login.controller.ts
@@ -1,28 +1,27 @@
 import { Request, Response } from 'express';
-import bcrypt, { hash } from 'bcrypt';
-import { getErrorMessage } from '../../utils/getErrorMessage';
-import { loginServices } from '../../services/auth/login.services';
-import jwt from 'jsonwebtoken';
 import { UserLoginType } from '../../Schemas/auth/auth.schema';
-import generateToken from '../../utils/auth/generateToken';
+import { loginServices } from '../../services/auth/login.services';
+import { validatePassword } from '../../utils/auth/validatePassword';
+import { generateToken } from '../../utils/auth/generateToken';
+import bcrypt from 'bcrypt';
 
 
-export default async function loginController(req: Request<unknown, unknown, UserLoginType>, res: Response) {
+export default async function loginController({ body }: Request<unknown, unknown, UserLoginType>, res: Response) {
     try {
-        loginServices(req.body).then( async user => {
+        loginServices(body).then(async user => {
             const hash = await bcrypt.hash(user?.password!, 10); // Temporal!! Porque las contraseñas no se guardan encriptadas y al hacer el compare no funciona
-            if (await bcrypt.compare(req.body.password, hash!)) {
-                const token = generateToken({ id: user?.id, role: user?.rol.nameRol});
+            if (await validatePassword(body.password, hash)) {
+                const token = generateToken({ id: user?.id, role: user?.rol.name });
                 res.json({
                     user_id: user?.id,
-                    user_role: user?.rol.nameRol,
+                    user_role: user?.rol.name,
                     token: token
                 });
-            }else{
-                res.status(401).json({ msg: "Incorrect password" })
+            } else {
+                res.status(401).json({ message: "Incorrect password" })
             }
         });
     } catch (error) {
-        res.status(500).json(getErrorMessage(error));
+        res.status(500).json(error);
     }
 }
