97e5279064ffbb8d552abcebc06fb9a6f9ff6453
Emilianoo03 <79849513+Emilianoo03@users.noreply.github.com>
2022-09-23 01:22:19 -0300

    Create controllers and services to edit job offer (#10)

---
+++ src/api/v1/controllers/company/JobOffer/EditJobOffer.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { editJobOfferService } from '../../../services/company/JobOffer/editJobOffer.service';
+
+export default async function EditJobOfferController(req: Request, res: Response) {
+    try {
+        console.log("entre c");
+        const jobOffer = await editJobOfferService(req.body, Number(req.params.id));
+        res.status(200).json(jobOffer); 
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
---
+++ src/api/v1/services/company/JobOffer/EditJobOffer.service.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { JobOfferType } from "../../../Schemas/company/joboffer.schema";
+const prisma = new PrismaClient();
+
+export async function editJobOfferService(jobOffer: JobOfferType, id: number) {
+    try{
+
+        const test=  await prisma.joboffer.update({
+            where: {
+                id_job_offer_id_company_id_work_area: {
+                    id_job_offer: id,
+                    id_company: jobOffer.user.id,
+                    id_work_area: jobOffer.id_work_area,
+                },
+            },
+            data: {
+                name: jobOffer.name,
+                description: jobOffer.description,
+                start_hour: jobOffer.start_hour,
+                end_hour: jobOffer.end_hour,
+                modality: jobOffer.modality,
+                quotas: jobOffer.quotas,
+                id_work_area: jobOffer.id_work_area,
+            },
+        });
+        console.log(test);
+        return test;
+    } catch (error) {
+        console.log(error);
+    }
+}
