13bc770d49b7da090f791354f4ee2248b7e1393e
Emilianoo03 <79849513+Emilianoo03@users.noreply.github.com>
2022-10-17 10:06:30 -0300

    hot fix: rename status of Response Delete JobOffer

--- src/api/v1/controllers/JobOffer/DeleteJobOffer.controller.ts
+++ src/api/v1/controllers/JobOffer/DeleteJobOffer.controller.ts
@@ -4,7 +4,7 @@ import { deleteJobOfferService } from '../../services/jobOffer/DeleteJobOffer.se
 export default async function DeleteJobOfferController({params, body}: Request, res: Response) {
     try {
         const { status, message, jobOffer } = await deleteJobOfferService(Number(params.id), body.user.id);
-        res.status({status}).json({message, jobOffer});
+        res.status(status).json({message, jobOffer});
     } catch (error) {
         res.status(500).json(error);
     }
